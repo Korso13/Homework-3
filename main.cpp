d6a94c258be2136b2e3ed42a812775211aad9130
Korso13 <101480776+Korso13@users.noreply.github.com>
2022-05-23 17:15:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,44 @@
 #include <iostream>
+
+class Figure
+{
+  public:
+  Figure() virtual {}
+  
+  int virtual area() = 0;
+};
+
+class Circle : public Figure
+{
+private:
+double radius;
+
+public:
+Circle() : {}
+
+int area() overrride {}
+};
+
+class Parallelogram : public Figure
+{
+};
+
+class Rectangle : public Parallelogram
+{
+};
+
+class Square : public Parallelogram
+{
+};
+
+class Rhombus : public Parallelogram
+{
+};
+
+
+int main()
+{
+  
+  
+  return 0;
+}
